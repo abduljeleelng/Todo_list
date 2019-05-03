7fad4e89ba4d4f02e67b2a8f3b1a2659653a12b6
abduljeleelng <abduljeleelng@gmail.com>
2019-05-03 08:48:46 +0100

    initial commit

--- app/src/main/java/ng/com/nsst/todo/MainActivity.java
+++ app/src/main/java/ng/com/nsst/todo/MainActivity.java
@@ -2,12 +2,64 @@ package ng.com.nsst.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvItems = findViewById(R.id.lvItem);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("Ola");
+        items.add("Abduljeleel");
+
+        AdapterView.OnItemLongClickListener itemLongClickListener = new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        };
+
     }
+    public void AddItems(View v){
+        EditText newItems = findViewById(R.id.etNewItem);
+        String itemsTest  = newItems.getText().toString();
+        itemsAdapter.add(itemsTest);
+        newItems.setText("");
+    }
+    /*
+    AdapterView.OnItemLongClickListener itemLongClickListener = new AdapterView.OnItemLongClickListener() {
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+            items.remove(position);
+            itemsAdapter.notifyDataSetChanged();
+            return true;
+        }
+    };
+
+    public void OnLongPressDelete(){
+        lvItems.setOnLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View items, int pos, long id){
+                // itemsAdapter.notifyDataSetChanged();
+                //return true ;
+            }
+        });
+    }
+    */
+
 }
