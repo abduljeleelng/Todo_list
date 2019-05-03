345ca965b9d76dd925c2e72a19e831562715c8d3
abduljeleelng <abduljeleelng@gmail.com>
2019-05-03 21:22:06 +0100

    initial commit

--- app/src/main/java/ng/com/nsst/todo/MainActivity.java
+++ app/src/main/java/ng/com/nsst/todo/MainActivity.java
@@ -5,61 +5,99 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
+    Button saveButton;
+    EditText edItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        saveButton = findViewById(R.id.btnSave);
+        edItems = findViewById(R.id.etItems);
         lvItems = findViewById(R.id.lvItem);
+        readItems();
         items = new ArrayList<>();
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
         items.add("Ola");
         items.add("Abduljeleel");
+        saveButton.setOnClickListener(saveButtonListener);
 
-        AdapterView.OnItemLongClickListener itemLongClickListener = new AdapterView.OnItemLongClickListener() {
+        /*
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 items.remove(position);
                 itemsAdapter.notifyDataSetChanged();
+                Toast.makeText(getApplicationContext(),"Items Successfully deleted ",Toast.LENGTH_LONG).show();
                 return true;
             }
-        };
-
+        });
+        */
+        setUpListView();
     }
     public void AddItems(View v){
-        EditText newItems = findViewById(R.id.etNewItem);
+        EditText newItems = findViewById(R.id.etItems);
         String itemsTest  = newItems.getText().toString();
         itemsAdapter.add(itemsTest);
         newItems.setText("");
     }
-    /*
-    AdapterView.OnItemLongClickListener itemLongClickListener = new AdapterView.OnItemLongClickListener() {
+    private void setUpListView(){
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                        items.remove(position);
+                        itemsAdapter.notifyDataSetChanged();
+                        writeItems();
+                        return true;
+                    }
+                }
+        );
+    }
+    public View.OnClickListener saveButtonListener = new View.OnClickListener() {
         @Override
-        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-            items.remove(position);
-            itemsAdapter.notifyDataSetChanged();
-            return true;
+        public void onClick(View v) {
+            if (edItems.getText().length()>0){
+                String items = edItems.getText().toString();
+                itemsAdapter.add(items);
+                writeItems();
+                edItems.setText("");
+            }
+            else
+              Toast.makeText(getApplicationContext(), "Enter Items ",Toast.LENGTH_LONG).show();
         }
     };
-
-    public void OnLongPressDelete(){
-        lvItems.setOnLongClickListener(new AdapterView.OnItemLongClickListener(){
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapter, View items, int pos, long id){
-                // itemsAdapter.notifyDataSetChanged();
-                //return true ;
-            }
-        });
+    private void readItems(){
+        File fileDir = getFilesDir();
+        File todoFile = new File(fileDir,"todo.text");
+        try{
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        }catch (IOException e){
+            items = new ArrayList<String>();
+        }
     }
-    */
-
+    private void writeItems(){
+        File fileDir = getFilesDir();
+        File todoFile = new File(fileDir,"todo.text");
+        try{
+            FileUtils.writeLines(todoFile,items);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
